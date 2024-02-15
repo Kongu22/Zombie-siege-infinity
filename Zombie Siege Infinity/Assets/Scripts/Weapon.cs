40bebbeb44b2a8532f90c342409ffa082bc8b7f0
Adi Shcherbakov <104269635+MrCIause@users.noreply.github.com>
2024-02-15 21:49:42 +0200

    Lesson 1+2

---
+++ Zombie Siege Infinity/Assets/Scripts/Weapon.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+   public GameObject bulletPrefab;
+   public Transform bulletSpawn;
+   public float bulletVelocity = 30;
+   public float bulletPrefabLifeTime = 3f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Left mouse click
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FireWeapon();
+        }
+        
+    }
+
+    private void FireWeapon()
+    {
+        //Instantiate the bullet
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+
+        //Shoot the bullet from the blue axis
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
+
+        //Destroy the bullet after a certain amount of time
+        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifeTime));
+    }
+
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(bullet);
+    }
+}
