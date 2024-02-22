7afbb88e116d347a2d8ac5061c0aedc5b17cc9ad
daninion <daninion12345@gmail.com>
2024-02-23 00:01:02 +0200

    more animations

--- Zombie Siege Infinity/Assets/SoundManager.cs
+++ Zombie Siege Infinity/Assets/SoundManager.cs
@@ -6,16 +6,26 @@ public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance { get;  set; }
 
+    public AudioSource ShootingChannel;
+
     public AudioSource emptyMagazineSound;
-    public AudioSource shootingSoundPistol92;
+
+    public AudioClip Pistol92Shot;
+    public AudioClip SAR2000Shot;
+    public AudioClip GlockShot;
+    public AudioClip ThompsonShot;
+    public AudioClip MP40Shot;
+    public AudioClip M4A4Shot;
+    public AudioClip AK47Shot;
+
     public AudioSource ReloadingSoundPistol92;
     public AudioSource ReloadingSoundScorpion;
-    public AudioSource shootingSoundSAR2000;
     public AudioSource ReloadingSoundSAR2000;
-    public AudioSource shootingSoundGlock;
     public AudioSource ReloadingSoundGlock;
-    public AudioSource shootingSoundM4A4;
+    public AudioSource ReloadingSoundThompson;
+    public AudioSource ReloadingSoundMP40;
     public AudioSource ReloadingSoundM4A4;
+    public AudioSource ReloadingSoundAK47;
 
 
     private void Awake()
@@ -35,29 +45,32 @@ public class SoundManager : MonoBehaviour
         switch(weaponModel)
         {
             case Weapon.WeaponModel.Pistol92:
-                shootingSoundPistol92.Play();
+                ShootingChannel.PlayOneShot(Pistol92Shot);
                 break;
             case Weapon.WeaponModel.SAR2000:
-                shootingSoundSAR2000.Play();
+                ShootingChannel.PlayOneShot(SAR2000Shot);
                 break;
             case Weapon.WeaponModel.Scorpion:
                 break;
             case Weapon.WeaponModel.Glock:
-                shootingSoundGlock.Play();
+                ShootingChannel.PlayOneShot(GlockShot);
                 break;
             case Weapon.WeaponModel.M500:
                 break;
             case Weapon.WeaponModel.Thompson:
+                ShootingChannel.PlayOneShot(ThompsonShot);
                 break;
             case Weapon.WeaponModel.M16:  
-                shootingSoundM4A4.Play();
+                ShootingChannel.PlayOneShot(M4A4Shot);
                 break;
             case Weapon.WeaponModel.M4A4:
-                shootingSoundM4A4.Play();
+                ShootingChannel.PlayOneShot(M4A4Shot);
                 break;
             case Weapon.WeaponModel.MP40:
+                ShootingChannel.PlayOneShot(MP40Shot);
                 break;
             case Weapon.WeaponModel.AK47:
+                ShootingChannel.PlayOneShot(AK47Shot);
                 break;
             case Weapon.WeaponModel.P90:
                 break;
@@ -89,6 +102,7 @@ public class SoundManager : MonoBehaviour
             case Weapon.WeaponModel.M500:
                 break;
             case Weapon.WeaponModel.Thompson:
+                ReloadingSoundThompson.Play();
                 break;
             case Weapon.WeaponModel.M16:  
                 ReloadingSoundM4A4.Play();
@@ -97,8 +111,10 @@ public class SoundManager : MonoBehaviour
                 ReloadingSoundM4A4.Play();
                 break;
             case Weapon.WeaponModel.MP40:
+                ReloadingSoundMP40.Play();
                 break;
             case Weapon.WeaponModel.AK47:
+                ReloadingSoundAK47.Play();
                 break;
             case Weapon.WeaponModel.P90:
                 break;
