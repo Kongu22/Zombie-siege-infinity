12a5506c582eef78be75816680582c08c18b150e
daniel <daninion12345@gmail.com>
2024-02-25 16:54:07 +0200

    Revert "MINI TIMING FIXES"
    This reverts commit 41836d35fa2c2512744be99ef6a0b371377050e2.

--- Zombie Siege Infinity/Assets/SoundManager.cs
+++ Zombie Siege Infinity/Assets/SoundManager.cs
@@ -11,7 +11,6 @@ public class SoundManager : MonoBehaviour
     public AudioSource emptyMagazineSound;
 
     public AudioClip Pistol92Shot;
-    public AudioClip ScorpionShot;
     public AudioClip SAR2000Shot;
     public AudioClip GlockShot;
     public AudioClip ThompsonShot;
@@ -59,7 +58,6 @@ public class SoundManager : MonoBehaviour
                 ShootingChannel.PlayOneShot(SAR2000Shot);
                 break;
             case Weapon.WeaponModel.Scorpion:
-                ShootingChannel.PlayOneShot(ScorpionShot);
                 break;
             case Weapon.WeaponModel.Glock:
                 ShootingChannel.PlayOneShot(GlockShot);
